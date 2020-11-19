d52a7348aa4534d718b895c1863234dc0e5cef56
bibliotekarz <arek@bibliotekarz.com>
2020-11-19 19:56:47 +0100

    każda reguła jest listą

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-import re
 from os import getenv
 from sys import argv
+
+slownik = {"name":"_spam", "enabled":"no", "type":"176", "action":"Move to folder"}
+def rozbij(lista):
+    for cos in lista:
+        if cos == "":
+            continue
+        cos_podzielone = cos.split("=")
+        # print("cos_podzielone1 " + cos_podzielone[0])
+        # print("cos_podzielone2 " + cos_podzielone[1].replace('"',''))
+        slownik[cos_podzielone[0]] = cos_podzielone[1].replace('"','').replace(") OR (all addresses,contains,", " ").replace("OR (all addresses,contains,", "")
+        # print(type(slownik))
+    print (slownik)
+
 
 if len(argv) > 1 :
     plik = argv[1]
@@ -25,8 +37,10 @@ licznik = 0
 
 while licznik < i:
     slownik_do_bazy = jedno_pole[licznik]
+    # print("slownik_do_bazy " + slownik_do_bazy)
     do_slownika = slownik_do_bazy.splitlines()
-    print(do_slownika)
+    # print(do_slownika)
+    rozbij(do_slownika)
     print(" ")
     licznik += 1
     
