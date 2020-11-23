f7a5418d25a3dd904969f2dbe8ae4c5bfeb1607c
bibliotekarz <arek@bibliotekarz.com>
2020-11-23 16:46:05 +0100

    ucywilizowac replace

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Wyciąga reguły filtrowania z thunderbirda do bazy danych sqlite
 ### budowa bazy danych
 
 cd /sql
+<br />
 sqlite3 defautlt.db < create_table.sql
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from os import getenv
 from sys import argv
 
@@ -9,24 +10,28 @@ def rozbij(lista):
         cos_podzielone = cos.split("=")
         # print("cos_podzielone1 " + cos_podzielone[0])
         # print("cos_podzielone2 " + cos_podzielone[1].replace('"',''))
+
+
         slownik[cos_podzielone[0]] = cos_podzielone[1].replace('"','').replace(") OR (all addresses,contains,", " ").replace("OR (all addresses,contains,", "").replace(") OR (from,contains,", " ").replace("OR (from,contains,", "").replace(") OR (\\return-path\\,contains,", " ").replace(") OR (\\x-wp-ip\\,contains,", " ").replace(") OR (\\x-originator\\,contains,", " ").replace(") OR (\\x-originating-ip\\,contains,"," ").replace(") OR (\\received\\,contains,", " ")
         # print(type(slownik))
+
+
         
     print (slownik)
     print(" ")
-    print(slownik.keys())
 
 
 if len(argv) > 1 :
     plik = argv[1]
-#    print("więcej " + plik)
 else :
     plik = input("wprowadź ścieżkę i nazwę pliku: ")
-#    print("mniej " + plik)
+
 
 # # tylko na czas testów
-plik = "source\\beka_msgFilterRules.txt"
-#plik = "source\msgFilterRules.txt"
+pobierz = "source/beka_msgFilterRules.txt"
+# pobierz = "source/msgFilterRules.txt"
+fileDir = os.path.dirname(os.path.realpath(__file__))
+plik = os.path.join(fileDir, pobierz)
 
 f = open(plik, "r", encoding='utf8')
 
@@ -40,9 +45,7 @@ licznik = 0
 
 while licznik < i:
     slownik_do_bazy = jedno_pole[licznik]
-    # print("slownik_do_bazy " + slownik_do_bazy)
     do_slownika = slownik_do_bazy.splitlines()
-    # print(do_slownika)
     rozbij(do_slownika)
     licznik += 1
     
